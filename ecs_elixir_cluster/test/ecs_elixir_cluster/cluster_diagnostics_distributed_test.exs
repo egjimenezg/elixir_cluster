091919c65cab66bb9b7534c1d3f1f9a6e4648af3
Gamaliel Jimenez <egjimenezg@gmail.com>
2026-03-15 01:29:31 -0600

    test: start distributed node before spawning peers

--- ecs_elixir_cluster/test/ecs_elixir_cluster/cluster_diagnostics_distributed_test.exs
+++ ecs_elixir_cluster/test/ecs_elixir_cluster/cluster_diagnostics_distributed_test.exs
@@ -2,14 +2,28 @@ defmodule EcsElixirCluster.ClusterDiagnosticsDistributedTest do
   use ExUnit.Case, async: false
   alias EcsElixirCluster.ClusterDiagnostics
 
+  defp peer_cookie_arg() do
+    Node.get_cookie()
+    |> Atom.to_string()
+    |> String.to_charlist()
+  end
+
   setup do
-    cookie = Node.get_cookie()
+    :ok = ensure_distributed_node!()
+    cookie = peer_cookie_arg()
+    unique_id = System.unique_integer([:positive])
 
     {:ok, peer_1, node_1} =
-      :peer.start_link(%{name: :peer_1, cookie: cookie})
+      :peer.start_link(%{
+        name: "peer_1_#{unique_id}",
+        args: [~c"-setcookie", cookie]
+      })
 
     {:ok, peer_2, node_2} =
-      :peer.start_link(%{name: :peer_2, cookie: cookie})
+      :peer.start_link(%{
+        name: "peer_2_#{unique_id}",
+        args: [~c"-setcookie", cookie]
+      })
 
     on_exit(fn ->
       for peer <- [peer_1, peer_2] do
@@ -32,5 +46,17 @@ defmodule EcsElixirCluster.ClusterDiagnosticsDistributedTest do
     connected = MapSet.new(status.connected_nodes)
 
     assert MapSet.subset?(MapSet.new([node_1, node_2]), connected)
+  end
+
+  defp ensure_distributed_node! do
+    if Node.alive?() do
+      :ok
+    else
+      case :net_kernel.start([:cluster_test, :shortnames]) do
+        {:ok, _pid} -> :ok
+        {:error, {:already_started, _pid}} -> :ok
+        {:error, reason} -> raise "could not start distributed node: #{inspect(reason)}"
+      end
+    end
   end
 end
